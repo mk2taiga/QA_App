17c019559f3ca0826b3947fdbbf70b8391b61995
宮崎大芽 <taigamiyazaki@miyazakitaigas-MacBook-Pro.local>
2017-05-11 20:18:00 +0900

    お気に入りボタン２

--- app/src/main/java/jp/techacademy/taiga/miyazaki/qa_app/QuestionDetailListAdapter.java
+++ app/src/main/java/jp/techacademy/taiga/miyazaki/qa_app/QuestionDetailListAdapter.java
@@ -1,8 +1,10 @@
 package jp.techacademy.taiga.miyazaki.qa_app;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,8 +14,15 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.api.model.StringList;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.io.ByteArrayOutputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class QuestionDetailListAdapter extends BaseAdapter {
 
@@ -32,6 +41,7 @@ public class QuestionDetailListAdapter extends BaseAdapter {
     String name;
     String body;
     byte[] bytes;
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
     //ここまで
 
     public QuestionDetailListAdapter(Context context, Question question) {
@@ -70,11 +80,12 @@ public class QuestionDetailListAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
         if (getItemViewType(position) == TYPE_QUESTION) {
             if (convertView == null) {
                 convertView = mLayoutInflater.inflate(R.layout.list_question_detail, parent, false);
             }
+
             bytes = mQustion.getImageBytes();
             body = mQustion.getBody();
             name = mQustion.getName();
@@ -85,35 +96,57 @@ public class QuestionDetailListAdapter extends BaseAdapter {
             TextView nameTextView = (TextView) convertView.findViewById(R.id.nameTextView);
             nameTextView.setText(name);
 
-            // ボタンの追加
-            likeButton = (Button) convertView.findViewById(R.id.likeButton);
-            likeButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                  // 保存のイメージは、Likesテーブルに各ユーザーごとのお気に入りcontentsを保存するイメージです。
-                    uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
-                    mDatabaseReference = FirebaseDatabase.getInstance().getReference();
-                    likeRef = mDatabaseReference.child(Const.LikesPATH).child(uid).child(mQustion.getQuestionUid());
-                    mLikeRef = mDatabaseReference.child(Const.LikesPATH).child(uid);
-                    if (likeRef == null) {
-//                        Firebaseに保存する
-                        //一つずつデータを取り出すという方法もやってはみましたが、answerまでを保存することはできなかった。
-                        // ユーザーid直下にコンテンツidを保存することができず、現在に至っています。
-                        mLikeRef.push(mQustion);
-                        likeButton.setText("いいね!済み");
-                    }else  {
-                        likeRef.removeValue();
-                        likeButton.setText("いいね!");
-                    }
-                }
-            });
-            //ここまで
-
             if (bytes.length != 0) {
                 Bitmap image = BitmapFactory.decodeByteArray(bytes, 0, bytes.length).copy(Bitmap.Config.ARGB_8888, true);
                 ImageView imageView = (ImageView) convertView.findViewById(R.id.imageView);
                 imageView.setImageBitmap(image);
             }
+
+            // ボタンの追加
+            likeButton = (Button) convertView.findViewById(R.id.likeButton);
+            likeButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // 保存のイメージは、Likesテーブルに各ユーザーごとのお気に入りcontentsを保存するイメージです。
+                    if (user != null) {
+                        uid = user.getUid();
+                        mDatabaseReference = FirebaseDatabase.getInstance().getReference();
+                        likeRef = mDatabaseReference.child(Const.LikesPATH).child(uid).child(mQustion.getQuestionUid());
+
+//                        if (likeRef == null) {
+//                        Firebaseに保存する
+                            Map<String, Object> data = new HashMap<String, Object>();
+                            data.put("body", body);
+                            data.put("title", mQustion.getTitle());
+                            data.put("name", name);
+                            data.put("uid", uid);
+
+                            if (bytes.length != 0) {
+                                Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length).copy(Bitmap.Config.ARGB_8888, true);
+                                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                                bitmap.compress(Bitmap.CompressFormat.JPEG, 80, baos);
+                                String bitmapString = Base64.encodeToString(baos.toByteArray(), Base64.DEFAULT);
+                                data.put("image", bitmapString);
+                            }
+
+                            if(mQustion.getAnswers() != null) {
+                                for (Object key : mQustion.getAnswers()) {
+                                    data.put("answer", key);
+                                }
+                            }
+
+
+                            likeRef.setValue(data);
+                            likeButton.setText("いいね!済み");
+//                        } else {
+//                            likeRef.removeValue();
+//                            likeButton.setText("いいね!");
+//                        }
+                    }
+                }
+            });
+            //ここまで
+
         } else {
             if (convertView == null) {
                 convertView = mLayoutInflater.inflate(R.layout.list_answer, parent, false);
